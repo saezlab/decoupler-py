73327233f67cc873e04ba4a6805e95a7ba379236
PauBadiaM <paubadiamompel@gmail.com>
2021-11-16 14:27:17 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="decoupler",
+    version="0.0.1",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
