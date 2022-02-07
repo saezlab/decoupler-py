3f74ba1c0ed0ccf43b0f9734d19f2ac13ae36366
PauBadiaM <paubadiamompel@gmail.com>
2022-02-07 10:21:07 +0100

    Removed access to raw methods

--- decoupler/__init__.py
+++ decoupler/__init__.py
@@ -1,16 +1,16 @@
 from .pre import extract, match, rename_net, get_net_mat, filt_min_n
 from .utils import melt, show_methods, check_corr, get_acts, get_toy_data
-from .method_wmean import run_wmean, wmean
-from .method_wsum import run_wsum, wsum
-from .method_ulm import run_ulm, ulm
-from .method_mlm import run_mlm, mlm
-from .method_ora import run_ora, ora
-from .method_mdt import run_mdt, mdt
-from .method_udt import run_udt, udt
-from .method_gsva import run_gsva, gsva
-from .method_gsea import run_gsea, gsea
-from .method_viper import run_viper, viper
-from .method_aucell import run_aucell, aucell
+from .method_wmean import run_wmean
+from .method_wsum import run_wsum
+from .method_ulm import run_ulm
+from .method_mlm import run_mlm
+from .method_ora import run_ora
+from .method_mdt import run_mdt
+from .method_udt import run_udt
+from .method_gsva import run_gsva
+from .method_gsea import run_gsea
+from .method_viper import run_viper
+from .method_aucell import run_aucell
 from .decouple import decouple
 from .consensus import run_consensus
 from .omnip import show_resources, get_resource, get_progeny
