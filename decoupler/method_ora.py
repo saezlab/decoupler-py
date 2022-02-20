63f366c1431ea705a7319c9e4308746f51ba80c0
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-02-20 16:54:58 +0100

    Cleaned ora code

--- decoupler/method_ora.py
+++ decoupler/method_ora.py
@@ -1,16 +1,15 @@
 """
 Method ORA.
-Code to run the Over Representation Analysis (ORA) method. 
+Code to run the Over Representation Analysis (ORA) method.
 """
 
 import numpy as np
 import pandas as pd
 
-from numpy.random import default_rng
 from scipy.stats import rankdata
 from math import log, exp, lgamma
 
-from .pre import extract, match, rename_net, filt_min_n
+from .pre import extract, rename_net, filt_min_n
 
 from anndata import AnnData
 from tqdm import tqdm
@@ -18,7 +17,7 @@ from tqdm import tqdm
 import numba as nb
 
 
-@nb.njit(nb.f4(nb.i4,nb.i4,nb.i4,nb.i4), cache=True)
+@nb.njit(nb.f4(nb.i4, nb.i4, nb.i4, nb.i4), cache=True)
 def mlnTest2r(a, ab, ac, abcd):
     if 0 > a or a > ab or a > ac or ab + ac > abcd + a:
         raise ValueError('invalid contingency table')
@@ -44,9 +43,9 @@ def mlnTest2r(a, ab, ac, abcd):
                 break
             sr = sr_new
         return max(0, pa - p0 - log(sr))
-    
-    
-@nb.njit(nb.f4(nb.i4,nb.i4,nb.i4,nb.i4), cache=True)
+
+
+@nb.njit(nb.f4(nb.i4, nb.i4, nb.i4, nb.i4), cache=True)
 def test1r(a, b, c, d):
     """
     Code adapted from:
@@ -57,30 +56,31 @@ def test1r(a, b, c, d):
 
 @nb.njit(nb.f4[:](nb.i4[:], nb.i4[:], nb.i4[:], nb.i4[:], nb.i4), parallel=True, cache=True)
 def get_pvals(sample, net, starts, offsets, n_background):
-    
+
+    # Init vals
     sample = set(sample)
     n_fsets = offsets.shape[0]
     pvals = np.zeros(n_fsets, dtype=nb.f4)
-    
     for i in nb.prange(n_fsets):
+
         # Extract feature set
         srt = starts[i]
         off = offsets[i] + srt
         fset = set(net[srt:off])
-        
+
         # Build table
         a = len(sample.intersection(fset))
         b = len(fset.difference(sample))
         c = len(sample.difference(fset))
         d = n_background - a - b - c
-        
+
+        # Store
         pvals[i] = test1r(a, b, c, d)
-        
     return pvals
 
 
 def ora(mat, net, n_up_msk, n_bt_msk, n_background=20000, verbose=False):
-    
+
     # Flatten net and get offsets
     offsets = net.apply(lambda x: len(x)).values.astype(np.int32)
     net = np.concatenate(net.values)
@@ -89,27 +89,25 @@ def ora(mat, net, n_up_msk, n_bt_msk, n_background=20000, verbose=False):
     starts = np.zeros(offsets.shape[0], dtype=np.int32)
     starts[1:] = np.cumsum(offsets)[:-1]
 
+    # Init empty
     pvls = np.zeros((mat.shape[0], offsets.shape[0]), dtype=np.float32)
     ranks = np.arange(mat.shape[1], dtype=np.int32)
-
     for i in tqdm(range(mat.shape[0]), disable=not verbose):
+
+        # Find ranks
         sample = rankdata(mat[i].A, method='ordinal').astype(np.int32)
         sample = ranks[(sample > n_up_msk) | (sample < n_bt_msk)]
 
         # Estimate pvals
         pvls[i] = get_pvals(sample, net, starts, offsets, n_background)
-    
     return pvls
 
 
-def run_ora(mat, net, source='source', target='target', n_up=None, 
-            n_bottom=0, n_background=20000, min_n=5, seed=42, 
+def run_ora(mat, net, source='source', target='target', n_up=None, n_bottom=0, n_background=20000, min_n=5, seed=42,
             verbose=False, use_raw=True):
     """
     Over Representation Analysis (ORA).
-    
     Wrapper to run ORA.
-    
     Parameters
     ----------
     mat : list, pd.DataFrame or AnnData
@@ -135,49 +133,50 @@ def run_ora(mat, net, source='source', target='target', n_up=None,
         Whether to show progress.
     use_raw : bool
         Use raw attribute of mat if present.
-    
     Returns
     -------
-    Returns ora activity estimates (-log10(p-values)) and p-values 
-    or stores them in `mat.obsm['ora_estimate']` and 
+    Returns ora activity estimates (-log10(p-values)) and p-values
+    or stores them in `mat.obsm['ora_estimate']` and
     `mat.obsm['ora_pvals']`.
     """
-    
+
     # Extract sparse matrix and array of genes
     m, r, c = extract(mat, use_raw=use_raw, verbose=verbose)
-    
+
     # Set up/bottom masks
     if n_up is None:
         n_up = np.ceil(0.05*len(c))
-    assert 0 <= n_up, 'n_up needs to be a value higher than 0.'
-    assert 0 <= n_bottom, 'n_bottom needs to be a value higher than 0.'
-    assert 0 <= n_background, 'n_background needs to be a value higher than 0.'
-    assert (len(c) - n_up) >= n_bottom, 'n_up and n_bottom overlap, please decrase the value of any of them.'
+    if not 0 <= n_up:
+        raise ValueError('n_up needs to be a value higher than 0.')
+    if not 0 <= n_bottom:
+        raise ValueError('n_bottom needs to be a value higher than 0.')
+    if not 0 <= n_background:
+        raise ValueError('n_background needs to be a value higher than 0.')
+    if not (len(c) - n_up) >= n_bottom:
+        raise ValueError('n_up and n_bottom overlap, please decrase the value of any of them.')
     n_up_msk = len(c) - n_up
     n_bt_msk = n_bottom + 1
-    
-    
+
     # Transform net
     net = rename_net(net, source=source, target=target, weight=None)
     net = filt_min_n(c, net, min_n=min_n)
-    
+
     # Transform targets to indxs
-    table = {name:i for i,name in enumerate(c)}
+    table = {name: i for i, name in enumerate(c)}
     net['target'] = [table[target] for target in net['target']]
     net = net.groupby('source')['target'].apply(lambda x: np.array(x, dtype=np.int32))
-    
     if verbose:
         print('Running ora on mat with {0} samples and {1} targets for {2} sources.'.format(m.shape[0], len(c), len(net)))
-    
+
     # Run ORA
     pvals = ora(m, net, n_up_msk, n_bt_msk, n_background, verbose)
-        
+
     # Transform to df
     pvals = pd.DataFrame(pvals, index=r, columns=net.index)
     pvals.name = 'ora_pvals'
     estimate = pd.DataFrame(-np.log10(pvals), index=r, columns=pvals.columns)
     estimate.name = 'ora_estimate'
-    
+
     # AnnData support
     if isinstance(mat, AnnData):
         # Update obsm AnnData object
