b3d7e0315dac0038fbad57fb422ec028d8bcaece
PauBadiaM <pau.badia@uni-heidelberg.de>
2023-06-29 14:54:33 +0200

    Modified gsea NES score and added get_gsea_df

--- decoupler/method_gsea.py
+++ decoupler/method_gsea.py
@@ -7,9 +7,10 @@ import numpy as np
 import pandas as pd
 
 from numpy.random import default_rng
-from scipy.stats import norm
+from scipy.sparse import csr_matrix
 
 from .pre import extract, rename_net, filt_min_n
+from .utils import p_adjust_fdr
 
 from anndata import AnnData
 from tqdm import tqdm
@@ -17,162 +18,270 @@ from tqdm import tqdm
 import numba as nb
 
 
-@nb.njit(nb.types.Tuple((nb.f4[:, :], nb.i8[:, :]))(nb.f4[:, :]), cache=True)
-def get_M_I(mat):
-    Idx = np.zeros(mat.shape, dtype=nb.i8)
-    for i in range(mat.shape[0]):
-        Idx[i] = np.argsort(-mat[i])
-    mat = np.abs(mat)
-    return mat, Idx
+@nb.njit(nb.types.Tuple((nb.f4, nb.i8, nb.f4[:]))(nb.f4[:], nb.i8[:], nb.b1[:], nb.f4), cache=True)
+def compute_es_per_rank(row, rnks, set_msk, dec):
+
+    # Init empty
+    mx_value = 0.0
+    cum_sum = 0.0
+    mx_pos = 0.0
+    mx_neg = 0.0
+    j_pos = 0
+    j_neg = 0
+    es = np.zeros(rnks.size, dtype=nb.f4)
+
+    # Compute norm
+    sum_set = np.sum(np.abs(row[set_msk]))
+    if sum_set == 0.:
+        return 0., 0, np.zeros(rnks.size, dtype=nb.f4)
+
+    # Compute ES
+    for i in rnks:
+        if set_msk[i]:
+            cum_sum += np.abs(row[i]) / sum_set
+            es[i] = cum_sum
+        else:
+            cum_sum -= dec
+            es[i] = cum_sum
+
+        # Update max scores and idx
+        if cum_sum > mx_pos:
+            mx_pos = cum_sum
+            j_pos = i
+        if cum_sum < mx_neg:
+            mx_neg = cum_sum
+            j_neg = i
+
+    # Determine if pos or neg are more enriched
+    if mx_pos > -mx_neg:
+        mx_value = mx_pos
+        j = j_pos
+    else:
+        mx_value = mx_neg
+        j = j_neg
+
+    return mx_value, j, es
 
 
 @nb.njit(nb.f4(nb.f4[:], nb.i8), cache=True)
 def std(arr, ddof):
     N = arr.shape[0]
     m = np.mean(arr)
-    var = np.sum((arr - m)**2)/(N-ddof)
+    var = np.sum((arr - m)**2) / (N - ddof)
     sd = np.sqrt(var)
     return sd
 
 
-@nb.njit(nb.f4(nb.f4[:], nb.i8[:], nb.i8, nb.i8[:], nb.i8[:], nb.i8, nb.f4), cache=True)
-def ks_sample(D, Idx, n_genes, geneset_mask, fset, n_geneset, dec):
+@nb.njit(nb.types.UniTuple(nb.f4, 2)(nb.f4[:], nb.i8[:], nb.b1[:], nb.f4, nb.f4, nb.i8, nb.i8), cache=True)
+def compute_nes_per_rank(row, rnks, set_msk, dec, es, times, seed):
 
-    sum_gset = 0.0
-    for i in nb.prange(n_geneset):
-        sum_gset += D[fset[i]]
+    # Keep old set_msk upstream
+    set_msk = set_msk.copy()
 
-    if sum_gset == 0.0:
-        return 0.0
-
-    mx_value = 0.0
-    cum_sum = 0.0
-    mx_pos = 0.0
-    mx_neg = 0.0
-
-    for i in nb.prange(n_genes):
-        idx = Idx[i]
-        if geneset_mask[idx] == 1:
-            cum_sum += D[idx] / sum_gset
-        else:
-            cum_sum -= dec
-
-        if cum_sum > mx_pos:
-            mx_pos = cum_sum
-        if cum_sum < mx_neg:
-            mx_neg = cum_sum
-
-    if np.round(mx_pos, 4) == np.round(-mx_neg, 4):
-        mx_value = 0.0
-    elif mx_pos > -mx_neg:
-        mx_value = mx_pos
-    else:
-        mx_value = mx_neg
-
-    return mx_value
-
-
-@nb.njit(nb.f4[:](nb.f4[:, :], nb.i8[:, :], nb.i8[:]), cache=True)
-def ks_matrix(D, Idx, fset):
-    n_samples, n_genes = D.shape
-    n_geneset = fset.shape[0]
-
-    geneset_mask = np.zeros(n_genes, dtype=nb.i8)
-    geneset_mask[fset] = 1
-
-    dec = 1.0 / (n_genes - n_geneset)
-
-    res = np.zeros(n_samples, dtype=nb.f4)
-    for i in nb.prange(n_samples):
-        res[i] = ks_sample(D[i], Idx[i], n_genes, geneset_mask, fset, n_geneset, dec)
-
-    return res
-
-
-@nb.njit(nb.f4[:](nb.f4[:, :], nb.i8[:, :], nb.i8[:], nb.f4[:], nb.i8[:, :]), cache=True)
-def ks_perms(D, Idx, fset, es, m_msk):
-    times = m_msk.shape[0]
-    if times == 0:
-        return es
-    res = np.zeros((times, D.shape[0]), dtype=nb.f4)
-    for i in nb.prange(times):
-        res[i] = ks_matrix(D, Idx[:, m_msk[i]], fset)
-
-    null_mean = np.zeros(D.shape[0], dtype=nb.f4)
-    null_std = np.zeros(D.shape[0], dtype=nb.f4)
-    for j in nb.prange(D.shape[0]):
-        null_mean[j] = res[:, j].mean()
-        null_std[j] = std(res[:, j], 1)
-
-    nes = (es - null_mean) / null_std
-
-    return nes
-
-
-@nb.njit(nb.types.UniTuple(nb.f4[:, :], 2)(nb.f4[:, :], nb.i8[:, :], nb.i8[:], nb.i8[:], nb.i8, nb.i8), parallel=True,
-         cache=True)
-def nb_gsea(D, Idx, net, offsets, times, seed):
-
+    # Compute null
     np.random.seed(seed)
-
-    n_samples = D.shape[0]
-    n_gsets = offsets.shape[0]
-    m_es = np.zeros((n_samples, n_gsets), dtype=nb.f4)
-    m_nes = np.zeros(m_es.shape, dtype=nb.f4)
-
-    # Generate random shuffling matrix
-    m_msk = np.zeros((times, D.shape[1]), dtype=nb.i8)
-    idxs = np.arange(D.shape[1])
+    null = np.zeros(times, dtype=nb.f4)
     for i in range(times):
-        np.random.shuffle(idxs)
-        m_msk[i] = idxs
+        np.random.shuffle(set_msk)
+        null[i], _, _ = compute_es_per_rank(row, rnks, set_msk, dec)
 
-    starts = np.zeros(n_gsets, dtype=nb.i8)
-    starts[1:] = np.cumsum(offsets)[:-1]
-    for j in nb.prange(n_gsets):
+    # Compute NES
+    pos_null_msk = null >= 0.
+    neg_null_msk = null < 0.
 
+    pos_null_mean = null[pos_null_msk].mean()
+    neg_null_mean = null[neg_null_msk].mean()
+
+    if es >= 0:
+        pval = (null[pos_null_msk] >= es).sum() / pos_null_msk.sum()
+        nes = es / pos_null_mean
+    else:
+        pval = (null[neg_null_msk] <= es).sum() / neg_null_msk.sum()
+        nes = -es / neg_null_mean
+
+    return nes, pval
+
+
+@nb.njit(nb.types.Tuple((nb.f4[:], nb.f4[:], nb.f4[:], nb.i8[:], nb.f4[:], nb.f4[:], nb.b1[:, :]))
+         (nb.f4[:], nb.i8[:], nb.i8[:], nb.i8[:], nb.i8, nb.i8, nb.b1), parallel=True, cache=True)
+def nb_gsea(row, net, starts, offsets, times, seed, ratios):
+
+    # Get dims
+    n_features, n_fsets = row.size, offsets.shape[0]
+
+    # Sort features
+    idx = np.argsort(-row)
+    row = row[idx]
+
+    # Get ranks
+    rnks = np.arange(n_features)
+
+    # Init empty
+    es = np.zeros(n_fsets, dtype=nb.f4)
+    nes = np.zeros(n_fsets, dtype=nb.f4)
+    pval = np.zeros(n_fsets, dtype=nb.f4)
+    sizes = np.zeros(n_fsets, dtype=nb.i8)
+    hits_r = np.zeros(n_fsets, dtype=nb.f4)
+    rnks_r = np.zeros(n_fsets, dtype=nb.f4)
+    le_msk = np.zeros((n_fsets, rnks.size), dtype=nb.b1)
+    for j in nb.prange(n_fsets):
+
+        # Extract fset
         srt = starts[j]
-        off = offsets[j] + srt
+        nf_in_set = offsets[j]
+        off = nf_in_set + srt
         fset = net[srt:off]
-        es = ks_matrix(D, Idx, fset)
-        m_es[:, j] = es
-        m_nes[:, j] = ks_perms(D, Idx, fset, es, m_msk)
 
-    return m_es, m_nes
+        # Get decending penalty
+        dec = 1.0 / (n_features - nf_in_set)
+
+        # Get msk
+        set_msk = np.zeros(n_features, dtype=nb.b1)
+        set_msk[fset] = True
+        set_msk = set_msk[idx]
+
+        # Compute es per feature
+        estimate, idx_max, v_es = compute_es_per_rank(row, rnks, set_msk, dec)
+        es[j] = estimate
+
+        # Compute nes
+        if times > 0:
+            nes[j], pval[j] = compute_nes_per_rank(row, rnks, set_msk, dec, estimate, times, seed)
+
+        # Compute Tag % and Feature %
+        if ratios:
+            size = set_msk.sum()
+            sizes[j] = size
+            if estimate >= 0:
+                sub_set_msk = set_msk[:idx_max + 1]
+                rnks_r[j] = (idx_max + 1) / v_es.size
+                hits_r[j] = sub_set_msk.sum() / size
+                set_msk[idx_max + 1:] = False
+                le_msk[j] = set_msk[np.argsort(idx)]
+            else:
+                sub_set_msk = set_msk[idx_max:]
+                rnks_r[j] = (v_es.size - idx_max) / v_es.size
+                hits_r[j] = sub_set_msk.sum() / size
+                set_msk[:idx_max] = False
+                le_msk[j] = set_msk[np.argsort(idx)]
+
+    return es, nes, pval, sizes, hits_r, rnks_r, le_msk
 
 
-def gsea(mat, net, times=1000, batch_size=10000, seed=42, verbose=False):
+def gsea(mat, net, times=1000, seed=42, verbose=False):
 
     # Flatten net and get offsets
     offsets = net.apply(lambda x: len(x)).values.astype(np.int64)
+    n_gsets = offsets.shape[0]
     net = np.concatenate(net.values)
 
-    # Get number of batches
+    # Define starts to subset offsets
+    starts = np.zeros(n_gsets, dtype=np.int64)
+    starts[1:] = np.cumsum(offsets)[:-1]
+
+    # Get dims
     n_samples = mat.shape[0]
     n_fsets = offsets.shape[0]
-    n_batches = int(np.ceil(n_samples / batch_size))
 
     # Init empty acts
     es = np.zeros((n_samples, n_fsets), dtype=np.float32)
     nes = np.zeros((n_samples, n_fsets), dtype=np.float32)
+    pvals = np.zeros((n_samples, n_fsets), dtype=np.float32)
 
-    for i in tqdm(range(n_batches), disable=not verbose):
+    for i in tqdm(range(n_samples), disable=not verbose):
 
-        # Subset batch
-        srt, end = i*batch_size, i*batch_size+batch_size
-        sub_mat = mat[srt:end].A
+        if isinstance(mat, csr_matrix):
+            row = mat[i].A[0]
+        else:
+            row = mat[i]
 
-        # Get modified m and I matrix
-        sub_mat, Idx = get_M_I(sub_mat)
-
-        # Compute GSEA per batch
-        es[srt:end], nes[srt:end] = nb_gsea(sub_mat, Idx, net, offsets, times, seed)
+        # Compute GSEA per row
+        es[i], nes[i], pvals[i], _, _, _, _ = nb_gsea(row, net, starts, offsets, times, seed, False)
 
     if times != 0:
-        pvals = norm.cdf(-np.abs(nes)) * 2
         return es, nes, pvals
     else:
         return es, None, None
+
+
+def get_gsea_df(df, stat, net, source='source', target='target', times=1000, min_n=5, seed=42, verbose=False):
+    """
+    Wrapper to run GSEA for results of differential analysis (long format dataframe).
+
+    Parameters
+    ----------
+    df : DataFrame
+        Long format DataFrame with features to be ranked. Assumes features are indexes.
+    stat : str
+        Name of the column containing the ranking metric.
+    net : DataFrame
+        Network in long format.
+    source : str
+        Column name in net with source nodes.
+    target : str
+        Column name in net with target nodes.
+    times : int
+        How many random permutations to do.
+    min_n : int
+        Minimum of targets per source. If less, sources are removed.
+    seed : int
+        Random seed to use.
+    verbose : bool
+        Whether to show progress.
+
+    Returns
+    -------
+    results : DataFrame
+        Results of GSEA.
+    """
+
+    # Extract
+    df = df.copy()
+    cols = df.columns
+    if stat not in cols:
+        raise ValueError('Column name "{0}" for stat not found in df. Please specify a valid column.'.format(stat))
+    m = df[stat].values.astype(np.float32)
+    c = df.index.values.astype('U')
+
+    # Transform net
+    net = rename_net(net, source=source, target=target, weight=None)
+    net = filt_min_n(c, net, min_n=min_n)
+
+    # Randomize feature order to break ties randomly
+    rng = default_rng(seed=seed)
+    idx = np.arange(m.size)
+    rng.shuffle(idx)
+    m, c = m[idx], c[idx]
+
+    # Transform targets to indxs
+    table = {name: i for i, name in enumerate(c)}
+    net['target'] = [table[target] for target in net['target']]
+    net = net.groupby('source')['target'].apply(lambda x: np.array(x, dtype=np.int64))
+
+    if verbose:
+        print('Running gsea on df with {0} targets for {1} sources.'.format(len(c), len(net)))
+
+    # Flatten net and get offsets
+    offsets = net.apply(lambda x: len(x)).values.astype(np.int64)
+    n_gsets = offsets.shape[0]
+    terms = net.index
+    net = np.concatenate(net.values)
+
+    # Define starts to subset offsets
+    starts = np.zeros(n_gsets, dtype=np.int64)
+    starts[1:] = np.cumsum(offsets)[:-1]
+
+    # Compute GSEA per row
+    es, nes, pvals, sizes, hits_r, rnks_r, le_msk = nb_gsea(m, net, starts, offsets, times, seed, True)
+
+    # Build final df
+    res = []
+    for i in range(es.size):
+        res.append([terms[i], es[i], nes[i], pvals[i], sizes[i], hits_r[i], rnks_r[i], ';'.join(c[le_msk[i]])])
+    res = pd.DataFrame(res, columns=['term', 'es', 'nes', 'nom_pval', 'set_size', 'tag_perc', 'rank_perc', 'leading_edge'])
+    res.insert(4, 'fdr_pval', p_adjust_fdr(res['nom_pval'].values))
+
+    return res
 
 
 def run_gsea(mat, net, source='source', target='target', times=1000, batch_size=10000, min_n=5, seed=42, verbose=False,
@@ -202,8 +311,7 @@ def run_gsea(mat, net, source='source', target='target', times=1000, batch_size=
     times : int
         How many random permutations to do.
     batch_size : int
-        Size of the samples to use for each batch. Increasing this will consume more
-        memmory but it will run faster.
+        Deprecated argument.
     min_n : int
         Minimum of targets per source. If less, sources are removed.
     seed : int
@@ -245,7 +353,7 @@ def run_gsea(mat, net, source='source', target='target', times=1000, batch_size=
         print('Running gsea on mat with {0} samples and {1} targets for {2} sources.'.format(m.shape[0], len(c), len(net)))
 
     # Run GSEA
-    estimate, norm_e, pvals = gsea(m, net, times=times, batch_size=batch_size, seed=seed, verbose=verbose)
+    estimate, norm_e, pvals = gsea(m, net, times=times, seed=seed, verbose=verbose)
 
     # Transform to df
     estimate = pd.DataFrame(estimate, index=r, columns=net.index)
