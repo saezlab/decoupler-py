ee46b805f0a7a7fd01534f98278418da8b5e8b23
deeenes <turei.denes@gmail.com>
2023-09-19 01:57:33 +0200

    Bump version: 1.5.0 → 1.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="decoupler",
-    version="1.5.0",
+    version="1.5.1",
     author="Pau Badia i Mompel",
     author_email="pau.badia@uni-heidelberg.de",
     description="Ensemble of methods to infer biological activities from omics data",
