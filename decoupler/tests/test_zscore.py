5955256a437e633aaf5ac73fcd427208766b039a
Sophia Müller-Dott <sophia.md@hotmail.de>
2024-07-24 11:10:54 +0200

    fixed column names

--- decoupler/tests/test_zscore.py
+++ decoupler/tests/test_zscore.py
@@ -24,7 +24,7 @@ def test_zscore():
 
 def test_run_zscore():
     m = np.array([[-7., -1., 1., 1.], [-4., -2., 1., 2.], [1., 2., 5., 1.], [1., 1., -6., -8.], [-8., -7., 1., 1.]])
-    r = np.array(['S1', 'S2', 'S3', 'S4'])
+    r = np.array(['S1', 'S2', 'S3', 'S4', 'S5'])
     c = np.array(['G1', 'G2', 'G3', 'G4'])
     df = pd.DataFrame(m, index=r, columns=c)
     net = pd.DataFrame([['T1', 'G1', 1], ['T1', 'G2', 1], ['T2', 'G3', -1], ['T2', 'G4', -1]],
