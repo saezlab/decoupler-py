3a2fabf97ffcbb06a0a3d532646053abfd371bb5
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-04-26 13:30:23 +0200

    Updated init

--- src/decoupler/__init__.py
+++ src/decoupler/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from . import ds, mt, op, pl, pp, tl
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ['ds', 'mt', 'op', 'pl', 'pp', 'tl']
 
-__version__ = version("decoupler")
+__version__ = version('decoupler')
