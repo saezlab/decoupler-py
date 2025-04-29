7ccbcd39a19b50bdb5485ab9b70f08a8ca056d57
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-04-29 17:41:55 +0200

    Added missing imports in pp

--- src/decoupler/pp/__init__.py
+++ src/decoupler/pp/__init__.py
@@ -1,3 +1,3 @@
 from .net import prune, adjmat, idxmat
 from .data import extract
-from .anndata import get_obsm, swap_layer, pseudobulk
+from .anndata import get_obsm, swap_layer, pseudobulk, filter_by_expr, filter_by_prop
