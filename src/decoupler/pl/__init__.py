c490b272b6bb0865768ebcc607d0855d714d7994
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-04-30 16:17:05 +0200

    Added plots to init

--- src/decoupler/pl/__init__.py
+++ src/decoupler/pl/__init__.py
@@ -1,1 +1,8 @@
-from .basic import BasicClass, basic_plot
+from decoupler.pl._barplot import barplot
+from decoupler.pl._dotplot import dotplot
+from decoupler.pl._filter_by_expr import filter_by_expr
+from decoupler.pl._filter_by_prop import filter_by_prop
+from decoupler.pl._leading_edge import leading_edge
+from decoupler.pl._network import network
+from decoupler.pl._source_targets import source_targets
+from decoupler.pl._volcano import volcano
