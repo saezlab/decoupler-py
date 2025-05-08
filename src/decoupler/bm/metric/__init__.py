d07844d04bc82cd7a567ea5a89034d73f6daa7f1
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-05-08 16:14:35 +0200

    Added bm metric module

---
+++ src/decoupler/bm/metric/__init__.py
@@ -0,0 +1,9 @@
+from decoupler.bm.metric._auc import auc
+from decoupler.bm.metric._fscore import fscore
+from decoupler.bm.metric._qrank import qrank
+
+dict_metric = {
+    'auc': auc,
+    'fscore': fscore,
+    'qrank': qrank,
+}
