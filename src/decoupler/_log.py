81fc595a21257aced662214bdca4bb1d7cbbb95b
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-05-15 20:45:17 +0200

    Changed log format

--- src/decoupler/_log.py
+++ src/decoupler/_log.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s | %(levelname)s: %(message)s",
+    format="%(asctime)s | [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
