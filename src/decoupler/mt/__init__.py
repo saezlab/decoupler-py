787be77f4e00dd60cfea63fced219fd206610115
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-05-15 20:53:20 +0200

    Removed reminder notes from mt init

--- src/decoupler/mt/__init__.py
+++ src/decoupler/mt/__init__.py
@@ -1,13 +1,13 @@
 from decoupler._Method import _show_methods
 from decoupler.mt._methods import aucell
-from decoupler.mt._methods import gsea  # p-vals are weird compared to old version
-from decoupler.mt._methods import gsva  # division by zero sometimes
+from decoupler.mt._methods import gsea
+from decoupler.mt._methods import gsva
 from decoupler.mt._methods import mdt
-from decoupler.mt._methods import mlm  # singular matrix
-from decoupler.mt._methods import ora  # Check that p-values are actually two sided and not one sided
+from decoupler.mt._methods import mlm
+from decoupler.mt._methods import ora
 from decoupler.mt._methods import udt
 from decoupler.mt._methods import ulm
-from decoupler.mt._methods import viper # TypeError: expected a sequence of integers or a single integer, got '146.0'
+from decoupler.mt._methods import viper
 from decoupler.mt._methods import waggr
 from decoupler.mt._methods import zscore
 from decoupler.mt._methods import _methods
