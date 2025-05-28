b8e2ae5bd9afb1a49bef2198605cf0c6e2d272a5
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-05-28 16:37:58 +0200

    Updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
+html_title = project_name
 html_logo = '_static/images/logo.png'
 html_favicon = '_static/images/logo.png'
 #html_title = project_name
