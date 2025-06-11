c229f5277c621e09ad9c084c0dd0765be823d127
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-06-11 20:41:03 +0200

    Updated copyright

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, str(HERE / "extensions"))
 info = metadata("decoupler")
 project_name = info["Name"]
 author = info["Author"]
-copyright = f"{datetime.now():%Y}, {author}."
+copyright = f"{datetime.now():%Y}, scverse"
 version = info["Version"]
 urls = dict(pu.split(", ") for pu in info.get_all("Project-URL"))
 repository_url = urls["Source"]
